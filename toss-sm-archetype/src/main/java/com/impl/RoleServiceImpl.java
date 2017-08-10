79917fb9d81d17424358dffd32328c9b1c1eb923
ville <ville.chen@rubansh.com>
2017-08-10 14:27:03 +0800

    druid连接池

--- toss-sm-archetype/src/main/java/com/impl/RoleServiceImpl.java
+++ toss-sm-archetype/src/main/java/com/impl/RoleServiceImpl.java
@@ -3,6 +3,7 @@ package com.impl;
 import com.dao.RoleMapper;
 import com.pojo.Role;
 import com.service.RoleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -14,6 +15,7 @@ import java.util.List;
 /**
  * Created by ville on 2017/7/18.
  */
+@Slf4j
 @Service
 public class RoleServiceImpl implements RoleService {
 
